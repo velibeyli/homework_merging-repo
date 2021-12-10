f406e38c7c0413acd9ef32753be13961d3b1fefa
velibeyli <valibayli91@gmail.com>
2021-12-10 18:18:36 +0400

    new repo created,gitignore file added, and kapital bank solutionadded

---
+++ KapitalBankApplication/KapitalBankApplication/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KapitalBankApplication
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+        public static void KapitalBankApp()
+        {
+            Console.WriteLine("This is Kapital bank Application");
+        }
+        public static void MobileApp()
+        {
+            Console.WriteLine("This is Mobile Application");
+        }
+    }
+}
